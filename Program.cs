97eb10f21a86349fde2b37bbf5fc5676fc546961
rajko-horvat <rajko.horvat@irb.hr>
2023-01-25 18:21:41 +0100

    Update imports

--- Program.cs
+++ Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using IRB.Revigo;
 using IRB.Revigo.Core;
 using IRB.Revigo.Databases;
 using IRB.Revigo.Worker;
-using static System.Net.WebRequestMethods;
 
 namespace RevigoCSExample
 {
